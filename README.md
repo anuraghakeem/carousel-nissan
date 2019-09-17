c72233b459bfe9ae0fa5a94011969bb50068f77a
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-09-17 12:31:04 +0530

    Updated Readme File, Specified entry point and output path

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# carousel-nissan
+# Webpage carousel : Nissan
+
+## Instructions
+1.  Clone this repo
+2.  Run `npm i`
+
+## Develpment: 
+Run `npm run start`, **localhost:8080** will open up in your default browser
+
+## Build: 
+Run `npm run build`
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path')
+const path = require("path");
 
 const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "./public/index.html",
@@ -7,38 +7,39 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
+  entry: "./src/index.js",
   output: {
-    path: path.resolve('dist'),
-    filename: 'bundled.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: "style-loader"
-            },
-            {
-              loader: "css-loader",
-              options: {
-                modules: true,
-                importLoaders: 1,
-                localIdentName: "[name]_[local]_[hash:base64]",
-                sourceMap: true,
-                minimize: true
-              }
-            }
-          ]
+    path: path.resolve("dist"),
+    filename: "bundled.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      ]
-    },
-    plugins: [htmlWebpackPlugin]
-  };
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin]
+};
